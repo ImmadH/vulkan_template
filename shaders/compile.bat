c699d815b4511aa38ef71672bb4a18942894bf7a
Immad <38505555+ImmadH@users.noreply.github.com>
2026-04-28 02:31:02 -0700

    shader updates

--- shaders/compile.bat
+++ shaders/compile.bat
@@ -1,3 +1,3 @@
-F:/VulkanSDK/1.4.321.1/Bin/glslc.exe shader.vert -o vert.spv
-F:/VulkanSDK/1.4.321.1/Bin/glslc.exe shader.frag -o frag.spv
+F:/VulkanSDK/1.4.341.1/Bin/glslc.exe shader.vert -o vert.spv
+F:/VulkanSDK/1.4.341.1/Bin/glslc.exe shader.frag -o frag.spv
 pause
